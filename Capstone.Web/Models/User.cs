1c91dedebca1fb1f5bc0010fee501d2fbc595e0a
hanna <https://hhailuht@bitbucket.org/te-cin-cohort-1/hannahailu-c-exercises.git>
2018-04-23 11:30:28 -0400

    User-model

--- Capstone.Web/Models/User.cs
+++ Capstone.Web/Models/User.cs
@@ -7,5 +7,11 @@ namespace Capstone.Web.Models
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; private set; }
+        public byte Permission { get; set; }
+
     }
 }
