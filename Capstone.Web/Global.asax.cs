a9cc00c8a698325676f9b87a197d1ae45eb5777a
Byron Thompson <fullstackbyron@gmail.com>
2018-04-23 11:04:46 -0400

    Initial Files

--- Capstone.Web/Global.asax.cs
+++ Capstone.Web/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -8,12 +10,29 @@ using System.Web.Routing;
 
 namespace Capstone.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : Ninject.Web.Common.NinjectHttpApplication
     {
-        protected void Application_Start()
+        protected override void OnApplicationStarted()
         {
+            base.OnApplicationStarted();
+
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        // Configure the dependency injection container.
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["SSGEEK"].ConnectionString;
+
+
+            // Set up the bindings
+            //kernel.Bind<IForumPostDAL>().To<ForumPostSqlDAL>().WithConstructorArgument("connectionString", connectionString);
+            //kernel.Bind<IProductDAL>().To<ProductSqlDAL>().WithConstructorArgument("connectionString", connectionString);
+
+            return kernel;
         }
     }
 }
