36fd26fa78aae05113c8c8352a1e8ca1e9bcb5c2
Denise Rudnay <diemahr@gmail.com>
2018-04-23 13:04:58 -0400

    latest PUSH - should have IAccountDAL and Register.cshtml in Account View folder

--- Capstone.Web/DAL/IAccountDAL.cs
+++ Capstone.Web/DAL/IAccountDAL.cs
@@ -7,12 +7,11 @@ using Capstone.Web.Models;
 
 namespace Capstone.Web.DAL
 {
-  public interface IAccountDAL
-  {
-   bool CreateUser(User user);
-    bool DeleteUser(string emailPK);
-    User GetUser(string emailPK);
-    bool UpdateUser(User user);
+    public interface IAccountDAL
+    {
+        bool CreateUser(User user);
+        bool DeleteUser(string emailPK);
+        User GetUser(string emailPK);
+        bool UpdateUser(User user);
 
-  }
-}
+    }
