82bfb5ee13acafc8cd2c076f3e7455d3d2f3037c
Byron Thompson <fullstackbyron@gmail.com>
2018-04-23 13:11:44 -0400

    Merged with new DB Schema

--- Capstone.Web/DAL/IAccountDAL.cs
+++ Capstone.Web/DAL/IAccountDAL.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using Capstone.Web.Models;
+﻿using Capstone.Web.Models;
 
 namespace Capstone.Web.DAL
 {
-  public interface IAccountDAL
-  {
-   bool CreateUser(User user);
-    bool DeleteUser(string emailPK);
-    User GetUser(string emailPK);
-    bool UpdateUser(User user);
+    public interface IAccountDAL
+    {
+        bool CreateUser(User user);
+        bool DeleteUser(string emailPK);
+        User GetUser(string emailPK);
+        bool UpdateUser(User user);
 
-  }
+    }
 }
