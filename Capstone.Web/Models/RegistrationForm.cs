5582b987085b8e228818319fcb7311b7c279bfee
hanna <https://hhailuht@bitbucket.org/te-cin-cohort-1/hannahailu-c-exercises.git>
2018-04-23 13:33:07 -0400

    registration Form

---
+++ Capstone.Web/Models/RegistrationForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class RegistrationForm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; private set; }
+        public string UserName { get; set; }
+
+    }
+}
