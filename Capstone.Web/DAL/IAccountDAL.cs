153c4c60fca3b3d5404878b0c2186af250b9bd9e
hanna <https://hhailuht@bitbucket.org/te-cin-cohort-1/hannahailu-c-exercises.git>
2018-04-23 12:24:45 -0400

    moderl-user

--- Capstone.Web/DAL/IAccountDAL.cs
+++ Capstone.Web/DAL/IAccountDAL.cs
@@ -10,7 +10,6 @@ namespace Capstone.Web.DAL
     public interface IAccountDAL
     {
         User GetUser(String emailPK);
-
         bool CreateUser(User user);
         bool UpdateUser(User user);
         bool DeleteUser(string emailPK);
