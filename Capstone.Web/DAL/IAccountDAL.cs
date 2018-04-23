92786801c526696cd7454739ff23451d4ca93d50
Denise Rudnay <diemahr@gmail.com>
2018-04-23 11:55:45 -0400

    sharing _Register View (what I have so far)

---
+++ Capstone.Web/DAL/IAccountDAL.cs
@@ -0,0 +1,16 @@
+﻿using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.DAL
+{
+  public interface IAccountDAL
+  {
+    User GetUser(string emailPK);
+    bool CreateUser(User user);
+    bool UpdateUser(User user);
+    bool DeleteUser(string emailPK);
+  }
+}
