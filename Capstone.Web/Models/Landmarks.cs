97cd60093a4f89a67bc44a322186d1fdd21307d6
hanna <https://hhailuht@bitbucket.org/te-cin-cohort-1/hannahailu-c-exercises.git>
2018-04-24 11:06:12 -0400

    landmarks-view

--- Capstone.Web/Models/Landmarks.cs
+++ Capstone.Web/Models/Landmarks.cs
@@ -5,13 +5,14 @@ using System.Web;
 
 namespace Capstone.Web.Models
 {
-    public class Landmark
+    public class Landmarks
     {
         public string Address { get; set; }
         public int ID { get; set; }
         public string Description { get; set; }
         public int Latitude { get; set; }
         public int Longtiude { get; set; }
+        public string Name { get; set; }
 
     }
 }
