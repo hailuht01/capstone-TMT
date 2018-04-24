c6fe133c78047f76aa49e336d4859cb878777215
Byron Thompson <fullstackbyron@gmail.com>
2018-04-24 12:53:56 -0400

    SQL

---
+++ Capstone.Web/DAL/ILandmarkDAL.cs
@@ -0,0 +1,17 @@
+﻿using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.DAL
+{
+    interface ILandmarkDAL
+    {
+        bool CreatLandmark();
+        Landmark GetLandmark();
+        bool UpdateLandmark();
+        bool DeleteLandmark();
+    }
+}
---
+++ Capstone.Web/DAL/ItineraryDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.DAL
+{
+    public class ItineraryDAL : IItineraryDAL
+    {
+        public bool CreatItinerary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteItinerary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Itinerary GetItenerary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateItinerary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
