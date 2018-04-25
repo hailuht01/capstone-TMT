8bbcf905fa4b4faa767b563b6cec4659086c786c
Byron Thompson <fullstackbyron@gmail.com>
2018-04-24 23:54:11 -0400

    ItineraryDAL

--- Capstone.Web/Controllers/AccountController.cs
+++ Capstone.Web/Controllers/AccountController.cs
@@ -8,36 +8,36 @@ using System.Web.Mvc;
 
 namespace Capstone.Web.Controllers
 {
-  public class AccountController : BaseController
-  {
-    IAccountDAL accountDAL;
-    public AccountController(IAccountDAL _accountDAL)
+    public class AccountController : BaseController
     {
-      this.accountDAL = _accountDAL;
-    }
+        IAccountDAL accountDAL;
+        public AccountController(IAccountDAL _accountDAL)
+        {
+          this.accountDAL = _accountDAL;
+        }
 
-    public ActionResult Index()
-    {
-      accountDAL.GetUser("userrrrr@citytour.com", "Password");
-      return View();
-    }
+        public ActionResult Index()
+        {
+          accountDAL.GetUser("userrrrr@citytour.com", "Password");
+          return View();
+        }
 
-    public ActionResult Login(string email, string password)
-    {
-      accountDAL.GetUser(email, password);
-      return View();
-    }
+        public ActionResult Login(string email, string password)
+        {
+          accountDAL.GetUser(email, password);
+          return View();
+        }
 
-    public ActionResult Register()
-    {
-      return View();
-    }
+        public ActionResult Register()
+        {
+          return View();
+        }
 
-    [HttpPost]
-    public ActionResult Register(RegistrationForm user)
-    {
-      accountDAL.CreateUser(user);
-      return RedirectToAction(Request.UrlReferrer.ToString());
+        [HttpPost]
+        public ActionResult Register(RegistrationForm user)
+        {
+          accountDAL.CreateUser(user);
+          return RedirectToAction(Request.UrlReferrer.ToString());
+        }
     }
-  }
 }
