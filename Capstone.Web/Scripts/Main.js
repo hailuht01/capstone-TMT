0886abfee3217595bb6e8bcb98d121c8aaad5659
Byron Thompson <fullstackbyron@gmail.com>
2018-04-25 15:22:53 -0400

    minor

--- Capstone.Web/Scripts/Main.js
+++ Capstone.Web/Scripts/Main.js
@@ -1,3 +1,6 @@
 ﻿$("document").ready(function () {
-
+    $.ajax({
+        url: "//www.google.com/maps/embed/v1/directions?origin=Tate+Modern & destination=Tower+of + London & waypoints=Ministry+of + sound & key=AIzaSyCPzAfumWS9n3IJ-PGos47STA1mp4QuLZQ",
+        
+    })
 })
