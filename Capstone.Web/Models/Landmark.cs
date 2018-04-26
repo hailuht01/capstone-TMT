27fbbddc12d5afaf20074359b29470a2ec2b7638
Byron Thompson <fullstackbyron@gmail.com>
2018-04-26 14:08:04 -0400

    idk

--- Capstone.Web/Models/Landmark.cs
+++ Capstone.Web/Models/Landmark.cs
@@ -8,7 +8,7 @@ namespace Capstone.Web.Models
   public class Landmark
   {
 
-    public string PlaceId { get; set; } // google placeId
+    public int ID { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
     public string Description { get; set; }
@@ -18,13 +18,13 @@ namespace Capstone.Web.Models
     public string QueryString => Name.Trim().Replace(' ', '+');
     public double Rating { get; set; }
 
-    public DateTime LastUpdated { get; set; }
+        public DateTime LastUpdated { get; set; }
 
     public Landmark() { }
 
-    public Landmark(string placeId, string Name, string Address, string Description, double Latitude, double Longitude, string PicName)
+    public Landmark(int ID, string Name, string Address, string Description, double Latitude, double Longitude, string PicName)
     {
-      this.PlaceId = placeId;
+      this.ID = ID;
       this.Name = Name;
       this.Address = Address;
       this.Description = Description;
@@ -35,17 +35,18 @@ namespace Capstone.Web.Models
 
     public static List<Landmark> GetSamples() => new List<Landmark>()
     {
-      new Landmark("ChIJ7Qfv_ZqzQYgRSV2kwAcCB4s", "Cincinnati Zoo", "3400 Vine St, Cincinnati, OH 45220", " The second oldest Zoo in the United States. Wide variety of animal collection.The Zoo was founded on 65 acres in the middle of the city, and since then has acquired some of the surrounding blocks and several reserves in Cincinnati’s suburbs.", 39.1446, 84.5086, "Cincinnati-Zoo.jpg"),
-      new Landmark("ChIJuZQ4_x20QYgR2QLQxS_400g", "Cincinnati Museum Center", "1301 Western Ave, Cincinnati, OH 45203", "Family Friendly Museum! This place its awesome! You are really going to love it.", 39.1152, 84.4900, "Cincinnati-Museum.jpg"),
-      new Landmark("ChIJi77WfdCzQYgRGbJHVBDHy-g", "Krohn Conservatory", "1501 Eden Park Dr, Cincinnati, OH 45202", "Blooming Conservatory! Because plants are super cool! Don't miss out.", 39.1100, 84.5378, "Krohn-Conservatory.jpg"),
-      new Landmark("ChIJT-TnLv6zQYgRElWrTagIgJ4", "Music Hall", "1241 Elm Street, Cincinnati, Ohio", " A classical music performance hall Don't miss out.", 39.1100, 84.5378, "Music-Hall.jpg"),
-      new Landmark("ChIJfVEQdNSzQYgRGubreDvtpcY", "Fountain Square", " 520 Vine St, Cincinnati, OH 45202", "Fountain Square is the city square; features many shops, restaurants, hotels, and offices.", 39.1100, 84.5378, "Fountain-Square.jpg"),
-      new Landmark("ChIJj5FhLG-xQYgRNhr-7ontbKo", "Newport On the Levee", "1 Levee Way, Newport, KY 41071", "Newport on the Levee is a premier dining and attraction destination!", 39.1100, 84.5378, "Newport-On-the-Levee.jpg"),
-      new Landmark("EjxKb2huIEEuIFJvZWJsaW5nIFN1c3BlbnNpb24gQnJpZGdlLCBDb3Zpbmd0b24sIEtZIDQxMDExLCBVU0E", "John A. Roebling Suspension Bridge","1501 Eden Park Dr, Cincinnati, OH 45202", "The John A. Roebling Suspension Bridge, originally known as the Cincinnati-Covington Bridge spans the Ohio River between Cincinnati, Ohio and Covington, Kentucky", 39.1100, 84.5378, "John A. Roebling Suspension Bridge.jpg"),
+      new Landmark(999997, "Cincinnati Zoo", "3400 Vine St, Cincinnati, OH 45220", " The second oldest Zoo in the United States. Wide variety of animal collection.The Zoo was founded on 65 acres in the middle of the city, and since then has acquired some of the surrounding blocks and several reserves in Cincinnati’s suburbs.", 39.1446, 84.5086, "Cincinnati-Zoo.jpg"),
+      new Landmark(999998, "Cincinnati Museum Center", "1301 Western Ave, Cincinnati, OH 45203", "Family Friendly Museum! This place its awesome! You are really going to love it.", 39.1152, 84.4900, "Cincinnati-Museum.jpg"),
+      new Landmark(999999, "Krohn Conservatory", "1501 Eden Park Dr, Cincinnati, OH 45202", "Blooming Conservatory! Because plants are super cool! Don't miss out.", 39.1100, 84.5378, "Krohn-Conservatory.jpg"),
+      new Landmark(999910, "Music Hall", "1241 Elm Street, Cincinnati, Ohio", " A classical music performance hall Don't miss out.", 39.1100, 84.5378, "Music-Hall.jpg"),
+
+    new Landmark(999911, "Fountain Square", " 520 Vine St, Cincinnati, OH 45202", "Fountain Square is the city square; features many shops, restaurants, hotels, and offices.", 39.1100, 84.5378, "Fountain-Square.jpg"),
+
+    new Landmark(999912, "Newport On the Levee", "1 Levee Way, Newport, KY 41071", "Newport on the Levee is a premier dining and attraction destination!", 39.1100, 84.5378, "Newport-On-the-Levee.jpg"),
+
+    new Landmark(999913, "John A. Roebling Suspension Bridge","1501 Eden Park Dr, Cincinnati, OH 45202", "The John A. Roebling Suspension Bridge, originally known as the Cincinnati-Covington Bridge spans the Ohio River between Cincinnati, Ohio and Covington, Kentucky", 39.1100, 84.5378, "John A. Roebling Suspension Bridge.jpg"),
 
     };
-
-
 
   }
 }
