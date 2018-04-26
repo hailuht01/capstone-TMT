ccb5650b8927de2918380657d38e1bd3fe48d544
Byron Thompson <fullstackbyron@gmail.com>
2018-04-26 09:00:08 -0400

    Markers

--- Capstone.Web/Scripts/Main.js
+++ Capstone.Web/Scripts/Main.js
@@ -1,6 +1,13 @@
 ﻿$("document").ready(function () {
     $.ajax({
-        url: "//www.google.com/maps/embed/v1/directions?origin=Tate+Modern & destination=Tower+of + London & waypoints=Ministry+of + sound & key=AIzaSyCPzAfumWS9n3IJ-PGos47STA1mp4QuLZQ",
-        
+        url: "www.google.com/maps/embed/v1/directions?origin=Tate+Modern&destination=Tower+of + London & waypoints=Ministry+of + sound & key=AIzaSyCPzAfumWS9n3IJ-PGos47STA1mp4QuLZQ",
+        method: "get",
+        dataType: "json",
+    }).done(function (results) {
+
     })
 })
+
+function getLandmarks() {
+    var baseURL = 
+}
