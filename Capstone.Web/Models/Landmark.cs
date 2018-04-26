7dce1b3b2181970703d4d7bbd7f63333c128d06f
Byron Thompson <fullstackbyron@gmail.com>
2018-04-26 11:36:29 -0400

    marker

--- Capstone.Web/Models/Landmark.cs
+++ Capstone.Web/Models/Landmark.cs
@@ -35,7 +35,7 @@ namespace Capstone.Web.Models
     public static List<Landmark> GetSamples() => new List<Landmark>()
     {
       new Landmark(999997, "Cincinnati Zoo", "3400 Vine St, Cincinnati, OH 45220", "Family Friendly Zoo! You are really going to love this place its awesome!", 39.144573, -84.508617, "Cincinnati-Zoo.jpg"),
-      new Landmark(999998, "Cincinnati Museum Center", "1301 Western Ave, Cincinnati, OH 45203", "Family Friendly Museum! This place its awesome! You are really going to love it.", 39.110019, 39.110019, "Cincinnati-Museum.jpg"),
+      new Landmark(999998, "Cincinnati Museum Center", "1301 Western Ave, Cincinnati, OH 45203", "Family Friendly Museum! This place its awesome! You are really going to love it.", 39.110019, -84.537781, "Cincinnati-Museum.jpg"),
       new Landmark(999999, "Krohn Conservatory", "1501 Eden Park Dr, Cincinnati, OH 45202", "Blooming Conservatory! Because plants are super cool! Don't miss out.", 39.115235, -84.489999, "Krohn-Conservatory.jpg")
     };
   }
