0f524789b3a2d4625c6b04dfa8d1ab24de2bc2bc
Denise Rudnay <diemahr@gmail.com>
2018-05-01 13:46:19 -0400

    commit for merge

--- Capstone.Web/DAL/ILandmarkDAL.cs
+++ Capstone.Web/DAL/ILandmarkDAL.cs
@@ -14,7 +14,8 @@ namespace Capstone.Web.DAL
         Landmark GetLandmark(string Placeid);
         List<Landmark> GetAllLandmarks();
         List<Landmark> GetAllLandmarks(int itinId);
+        List<Landmark> GetPopularLandmarks();
         bool UpdateLandmark(Landmark landmark);
-        bool DeleteLandmark(string id);
+        bool DeleteLandmark(string id);        
     }
 }
