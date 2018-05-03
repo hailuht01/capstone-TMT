3e30b1720b3b079df3cf3ac4f048a87ba4a3ee90
jakeneels <nizlmmk@gmail.com>
2018-05-03 13:46:29 -0400

    genroute db fix

--- Capstone.Web/Scripts/Main.js
+++ Capstone.Web/Scripts/Main.js
@@ -5,6 +5,8 @@ var landmarkArr = [];
 var activeItinIdIndex;
 var prevItinIdIndex;
 var mapToggle=true;
+var genRoute=false;
+var placeIds
 
 
 $("document").ready(function () {
@@ -14,79 +16,135 @@ $("document").ready(function () {
 
 });
 
-function initMap() {
-  //Map Options
-  var options = {
-    zoom: 13,
-    center: { lat: 39.103118, lng: -84.512020 },
-    clickableIcons: true,
-    gestureHandling: 'auto',
-  }
+if(!genRoute) {
 
-  map = new google.maps.Map(document.getElementById('map'), options);
-}
+    function initMap() {
+        //Map Options
+        var options = {
+            zoom: 13,
+            center: {lat: 39.103118, lng: -84.512020},
+            clickableIcons: true,
+            gestureHandling: 'auto',
+        }
 
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
+        map = new google.maps.Map(document.getElementById('map'), options);
+    }
 
-function AddMarker(props) {
-  var windowHtml = `<h3>${props.name}</h3>
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+
+    function AddMarker(props) {
+        var windowHtml = `<h3>${props.name}</h3>
                         <p>${props.address}</p>
                         <p>${props.description}</p>
                         <input id="markerId" type="hidden" value="${props.placeId}"/>
 `;
-  //Add Marker
-  var marker = new google.maps.Marker({
-    map: map,
-    draggable: false,
-    animation: google.maps.Animation.DROP,
-    position: props.coords,
-  });
+        //Add Marker
+        var marker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: props.coords,
+        });
 
-  //Add InfoWindow
-  var infoWindow = new google.maps.InfoWindow({
-    content: windowHtml
-  });
-  marker.addListener('mouseover', function () {
-    infoWindow.open(map, marker);
-  });
-  marker.addListener('mouseout', function () {
-    infoWindow.close();
-  });
+        //Add InfoWindow
+        var infoWindow = new google.maps.InfoWindow({
+            content: windowHtml
+        });
+        marker.addListener('mouseover', function () {
+            infoWindow.open(map, marker);
+        });
+        marker.addListener('mouseout', function () {
+            infoWindow.close();
+        });
 
-  //Add Modal Detail
-  marker.addListener('click', function () {
-   $('#landmark-detail-' + props.id).modal('show');
+        //Add Modal Detail
+        marker.addListener('click', function () {
+            $('#landmark-detail-' + props.id).modal('show');
 
 
-  $('.modal-title').text(props.name);
+            $('.modal-title').text(props.name);
 
-    genLandmarkModalHTML(props.placeId, props.description);
+            genLandmarkModalHTML(props.placeId, props.description);
 
-  });
+        });
+    }
+
+
+
+}else{
+
+
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 6,
+            center: {lat: 39.103118, lng: -84.512020},
+        });
+        directionsDisplay.setMap(map);
+
+        document.getElementById('submit').addEventListener('click', function() {
+            calculateAndDisplayRoute(directionsService, directionsDisplay);
+        });
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var waypts = [];
+        var checkboxArray = document.getElementById('waypoints');
+        for (var i = 0; i < checkboxArray.length; i++) {
+            if (checkboxArray.options[i].selected) {
+                waypts.push({
+                    location: checkboxArray[i].value,
+                    stopover: true
+                });
+            }
+        }
+
+        directionsService.route({
+            origin: document.getElementById('start').value,
+            destination: document.getElementById('end').value,
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: 'DRIVING'
+        }, function(response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+                var route = response.routes[0];
+                var summaryPanel = document.getElementById('directions-panel');
+                summaryPanel.innerHTML = '';
+                // For each route, display summary information.
+                for (var i = 0; i < route.legs.length; i++) {
+                    var routeSegment = i + 1;
+                    summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                        '</b><br>';
+                    summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+                    summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+                    summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+                }
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+
 }
 
 
 function ShowModal(props) {
 
-    //Add Modal Detail
-
-    //$('.landmark-item>').addListener('click', function () {
-    // $('#landmark-detail').modal('show');
-
     $(`#landmark-detail-${props.id}`).modal('show');
     $('#landmark-detail').modal('show');
     console.log(props.placeId);
 
-    $("#modal-title-"+props.id).text(props.name);
+    $("#modal-title-" + props.id).text(props.name);
     genLandmarkModalHTML(props);
 
-    //});
 }
 
 function genLandmarkModalHTML(props) {
@@ -113,7 +171,9 @@ function genLandmarkModalHTML(props) {
             var todaysHours; // html var
             try {
                 todaysHours = j.opening_hours['weekday_text'][days.indexOf(day)];
-            } catch (Exception) { console.warn("no open time for this place") }
+            } catch (Exception) {
+                console.warn("no open time for this place")
+            }
 
 
             var photoReference = j['photos'][0].photo_reference;
@@ -121,9 +181,9 @@ function genLandmarkModalHTML(props) {
             var photoQuery = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoReference + "&key=" + APIKey;
 
 
-            $("#modal-body-"+props.id).html(`
+            $("#modal-body-" + props.id).html(`
 <div class="row">
-<div class='col-sm-12 col-6'><img src='${photoQuery}'/></div><div class='col-sm-12 col-6'><b>Departure Date ${todaysHours}</b></div>
+<div class='col-sm-12 col-6'><img src='${photoQuery}'/></div><div class='col-sm-12 col-6'><b></b></div>
 
 <div class='col-sm-6 col-6'><b>${phone}</b></div> <div class='col-sm-6 col-3'><h5><a href='${website}' target="_blank">Website</a></h5></div>
 
@@ -136,74 +196,75 @@ function genLandmarkModalHTML(props) {
 
         .fail(function (data) {
             console.error("ajax fail" + error);
-            ("#modal-body-"+props.id).html("Offline Error");
+            ("#modal-body-" + props.id).html("Offline Error");
         });
 }
 
 
-
 function toggleActiveItin(itinId) {
-  activeItinIdIndex = itinId;
-  console.log("prev itin: " + prevItinIdIndex);
-  console.log("active itin: " + activeItinIdIndex);
-  $('#Itin-' + activeItinIdIndex + '>#activeItin').show();
-  if (activeItinIdIndex != prevItinIdIndex) {
-    $('#Itin-' + prevItinIdIndex + '>#activeItin').hide();
-  }
+    activeItinIdIndex = itinId;
+    console.log("prev itin: " + prevItinIdIndex);
+    console.log("active itin: " + activeItinIdIndex);
+    $('#Itin-' + activeItinIdIndex + '>#activeItin').show();
+    if (activeItinIdIndex != prevItinIdIndex) {
+        $('#Itin-' + prevItinIdIndex + '>#activeItin').hide();
+    }
 
-  prevItinIdIndex = activeItinIdIndex;
+    prevItinIdIndex = activeItinIdIndex;
 
 }
 
-function toggleMap(){
-    var hide='';
-    var show='';
+function toggleMap() {
+    var hide = '';
+    var show = '';
 
-        if(mapToggle) {
-             hide = '#map-container';
-             show = '#landmark-list';
-             mapToggle=false;
-        }
-        else{
-             hide = '#landmark-list';
-             show = '#map-container';
-             mapToggle=true;
-        }
+    if (mapToggle) {
+        hide = '#map-container';
+        show = '#landmark-list';
+        mapToggle = false;
+    }
+    else {
+        hide = '#landmark-list';
+        show = '#map-container';
+        mapToggle = true;
+    }
 
     $(hide).hide();
     $(show).show();
 }
 
 function addToItin(idStr, name) {
-  //$("#markerPlaceId").value();
-  {
+    //$("#markerPlaceId").value();
+    {
 
-    if (!landmarkArr.includes(idStr)) {
-      landmarkArr.push(idStr);
-      console.log("add to itin: " + idStr + "  itin: " + activeItinIdIndex);
-      console.warn(landmarkArr);
+        if (!landmarkArr.includes(idStr)) {
+            landmarkArr.push(idStr);
+            console.log("add to itin: " + idStr + "  itin: " + activeItinIdIndex);
+            console.warn(landmarkArr);
 
-      $('#itin-landmark-array-'+activeItinIdIndex).val( function(){
-          return this.value + " "+idStr
-      })
+            $('#itin-landmark-array-' + activeItinIdIndex).val(function () {
+                return this.value + " " + idStr
+            })
 
-      var landmarkHTML = `<div class=' col-8 btn-success round border' id='itin-landitem-id-${idStr}' data-value='${idStr}' onclick="removeLandmark(${idStr})">${name}</div>`;
+            var landmarkHTML = `<div class=' col-8 btn-success round border' id='itin-landitem-id-${idStr}' data-value='${idStr}' onclick="removeLandmark(${idStr})">${name}</div>`;
 
-        $('#activeItin').append(landmarkHTML);
+            $('#activeItin').append(landmarkHTML);
 
-    } else { console.log("already in itin land:" + idStr + "  itin: " + activeItinIdIndex) }
-    $('.landmark-detail').modal('hide');
+        } else {
+            console.log("already in itin land:" + idStr + "  itin: " + activeItinIdIndex)
+        }
+        $('.landmark-detail').modal('hide');
 
-  }
+    }
 
 }
 
 function createItin(newIndex) {
-  $('#Itin-' + activeItinIdIndex + '>#activeItin').hide();
-  $('#Itin-' + prevItinIdIndex + '>#activeItin').hide();
-  activeItinIdIndex = newIndex;
-  console.log("create itin new index" + activeItinIdIndex);
-  $('#createItinForm').show();
+    $('#Itin-' + activeItinIdIndex + '>#activeItin').hide();
+    $('#Itin-' + prevItinIdIndex + '>#activeItin').hide();
+    activeItinIdIndex = newIndex;
+    console.log("create itin new index" + activeItinIdIndex);
+    $('#createItinForm').show();
 }
 
 function saveItin(activeItinId, itinArr) {
@@ -212,22 +273,32 @@ function saveItin(activeItinId, itinArr) {
 
 function removeLandmark(idStr) { // remove from value and hide div
 //var val = document.getElementById('demo').getAttribute('data-value');
-      $('#itin-landmark-array-'+activeItinIdIndex).val( function(){
-          console.log("old " + this.value + "new" + this.value.replace(idStr, ''));
-          return this.value.replace(idStr, '');
-      })
+    $('#itin-landmark-array-' + activeItinIdIndex).val(function () {
+        console.log("old " + this.value + "new" + this.value.replace(idStr, ''));
+        return this.value.replace(idStr, '');
+    })
 
-    $('#itin-landitem-id-'+idStr).hide()
+    $('#itin-landitem-id-' + idStr).hide()
 
 }
 
 function removeItinerary(i) {
-  if (landmarkArr.length >= i - 1) {
-    confirm("Permanately Delete this Itinerary?");
-    landmarkArr.splice(i, 1);
-    console.log("removed Itin index= " + i);
-  }
+    if (landmarkArr.length >= i - 1) {
+        confirm("Permanately Delete this Itinerary?");
+        landmarkArr.splice(i, 1);
+        console.log("removed Itin index= " + i);
+    }
 }
+
+function generateRoute(placeIdStr)
+{
+
+    placeIds = placeIdStr.replace('  ', ' ' ).split(' ');
+    console.log(placeIds);
+    genRoute = true;
+    location.reload();
+}
+
 
 
 
